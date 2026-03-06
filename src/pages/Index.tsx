2343b8650f74dc120af7ebe46627268c29ec2612
eliasrockins <eliasmartinsvieira92@gmail.com>
2026-03-06 13:39:35 +0000

    Corrigindo syntax e aplicando cor

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -13,7 +13,7 @@ import WhatsAppPopup from "@/components/WhatsAppPopup";
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-background">
+    <div className="min-h-screen bg-[#01010C]">
       <Navbar />
       <HeroSection />
       <HomeVideoTestimonials />
